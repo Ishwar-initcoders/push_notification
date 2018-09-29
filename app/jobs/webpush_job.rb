db66b0d678836fa848c8aa938152a47d56dde9fe
ishwar-initcoders <ishwar@initcoders.com>
2018-09-29 15:55:58 +0530

    update google api key

--- app/jobs/webpush_job.rb
+++ app/jobs/webpush_job.rb
@@ -2,13 +2,12 @@ class WebpushJob < ActiveJob::Base
   queue_as :default
 
   def perform(message, params)
-
     puts "ishwar singh gehlot ============= #{params}"
     client = WebpushClient.new
 
     log("sending #{message} to #{params[:endpoint]}")
     response = client.send_notification(message, params)
-    log(response ? "success" : "failed")
+    log(response ? "success" : "failed")    
   end
 
   def log(message)
